da44c78e33ef79ada39cd51161a86659cea82e44
Marwan Mostafa <74471326+Marwan-Dev@users.noreply.github.com>
2022-08-20 22:03:06 +0200

    middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,40 @@
 import express, { Application, Request, Response } from 'express';
-
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { rateLimit } from 'express-rate-limit';
 const PORT = 3000;
 
 // Create Instance Server
 const app: Application = express();
-
+// Middleware to parse incomming requests
+app.use(express.json());
+// HTTP request logger middleware
+app.use(morgan('common'));
+// Rate limit for all requests
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    message: 'Too many requests from this IP, please try after one hour',
+  })
+);
+// HTTP request security middleware
+app.use(helmet());
 // Routing for / path
 app.get('/', (req: Request, res: Response) => {
   res.json({
     message: 'Hello World',
   });
 });
-
+// Post request
+app.post('/', (req: Request, res: Response) => {
+  res.json({
+    message: 'Hello World from Post Request',
+    data: req.body,
+  });
+});
 // Start Express Server
 app.listen(PORT, () => {
   console.log(`Server is running on Port: ${PORT}`);
